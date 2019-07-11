83611e006c029aa9775ea3e45c3455078e813447
ski96277 <imran.bu96.com>
2019-07-11 18:13:08 +0600

    add font style

--- app/src/main/java/com/example/supervizor/Fragment/Company/Calender_F.java
+++ app/src/main/java/com/example/supervizor/Fragment/Company/Calender_F.java
@@ -6,16 +6,25 @@ import android.graphics.Color;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.supervizor.Activity.CompanyMainActivity;
+import com.example.supervizor.JavaPojoClass.Event_details_PojoClass;
+import com.example.supervizor.Java_Class.CheckInternet;
+import com.example.supervizor.Java_Class.Check_User_information;
 import com.example.supervizor.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.kinda.alert.KAlertDialog;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
@@ -33,6 +42,8 @@ import es.dmoral.toasty.Toasty;
 
 public class Calender_F extends Fragment {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     MaterialCalendarView materialCalendarView;
 
@@ -62,8 +73,8 @@ public class Calender_F extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initialize(view);
 
-        materialCalendarView = view.findViewById(R.id.calendarView_ID);
 
         CalendarDay date = materialCalendarView.getCurrentDate();
         materialCalendarView.setBackgroundColor(Color.parseColor("#EFEFEF"));
@@ -75,33 +86,85 @@ public class Calender_F extends Fragment {
 
 
         materialCalendarView.setOnDateChangedListener((materialCalendarView, calendarDay, b) -> {
+
             if (materialCalendarView.getSelectedDate() != null) {
-                /*Toasty.info(getContext(),materialCalendarView.getSelectedDate().toString()).show();
-                materialCalendarView.setSelectionColor(Color.parseColor("#926C24"));
-*/
+
+                String date_child = calendarDay.getDate().toString();
+                String day_date = String.valueOf(calendarDay.getDay());
+                String month = String.valueOf(calendarDay.getMonth());
+                String year = String.valueOf(calendarDay.getYear());
+
                 final Dialog dialog = new Dialog(getActivity());
-//                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
                 dialog.setCancelable(false);
+
                 dialog.setContentView(R.layout.custom_alert_dialog_event_add);
-                Button button_ok = dialog.findViewById(R.id.button_ok_dialog);
-                TextView textView=dialog.findViewById(R.id.event_date_TV_ID);
-                textView.setText("Event : "+materialCalendarView.getSelectedDate().getDate());
-                button_ok.setOnClickListener(v -> {
+                Button add_Event_button = dialog.findViewById(R.id.button_ok_dialog);
+                Button cross_btn=dialog.findViewById(R.id.cross_image_button_ID);
+                TextView textView = dialog.findViewById(R.id.event_date_TV_ID);
+                EditText title_event_ET = dialog.findViewById(R.id.event_title_ET_ID);
+                EditText details_event_ET = dialog.findViewById(R.id.event_details_ET_ID);
+
+                textView.setText("Event : " + calendarDay.getDate());
+
+                cross_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                add_Event_button.setOnClickListener(v -> {
+                    String event_title = title_event_ET.getText().toString();
+                    String event_details = details_event_ET.getText().toString();
+
+                    Check_User_information check_user_information=new Check_User_information();
+                    String user_ID=check_user_information.getUserID();
+
+                    if (event_title.isEmpty() || event_details.isEmpty()){
+                        Toasty.info(getActivity(),"Fill up the input field").show();
+                        dialog.dismiss();
+                        return;
+                    }
+
+                    Event_details_PojoClass event_details_pojoClass
+                            = new Event_details_PojoClass(date_child,day_date,month,year,event_title,event_details);
+
+                    if (!CheckInternet.isInternet(getActivity())) {
+                        Toasty.error(getActivity(), "Internet Connection Error");
+                        return;
+                    }
                     dialog.dismiss();
 
-                    KAlertDialog kAlertDialog=new KAlertDialog(getActivity(),KAlertDialog.SUCCESS_TYPE);
-                    kAlertDialog.setTitleText("Done");
-
-                    kAlertDialog.setConfirmClickListener(kAlertDialog1 -> {
-                       kAlertDialog.dismissWithAnimation();
-                    });
+                    KAlertDialog kAlertDialog = new KAlertDialog(getActivity(), KAlertDialog.PROGRESS_TYPE);
+                    kAlertDialog.setTitleText("Saving Data to Database");
                     kAlertDialog.show();
+                    databaseReference.child("Event_list").child(user_ID).child(date_child).setValue(event_details_pojoClass).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                       Toasty.success(getContext(),"Event Saved").show();
+                       kAlertDialog.changeAlertType(KAlertDialog.SUCCESS_TYPE);
+                       kAlertDialog.setTitleText("Done");
+                       kAlertDialog.setConfirmClickListener(new KAlertDialog.OnSweetClickListener() {
+                           @Override
+                           public void onClick(KAlertDialog kAlertDialog) {
+                          kAlertDialog.dismissWithAnimation();
+                           }
+                       });
+                        }
+                    });
                 });
                 dialog.show();
 
             }
         });
 
+    }
+
+    private void initialize(View view) {
+        materialCalendarView = view.findViewById(R.id.calendarView_ID);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
 
     //set title
