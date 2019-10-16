9632cdf8d463fe5380b66e3eeffa1cdb5f4f4f4f
ski96277 <imran.bu96.com>
2019-10-16 18:13:28 +0600

    add Boom menu Button on home page

--- app/src/main/java/com/example/supervizor/Fragment/Company/Employee_list_F.java
+++ app/src/main/java/com/example/supervizor/Fragment/Company/Employee_list_F.java
@@ -28,6 +28,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.kinda.alert.KAlertDialog;
+import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Util;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Rect;
 
 import java.util.ArrayList;
 
@@ -39,21 +47,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import es.dmoral.toasty.Toasty;
 
+
 public class Employee_list_F extends Fragment {
 
-    private Button plus_icon_button;
+//    private Button plus_icon_button;
     private SearchView search_ET;
     //    private Spinner spinner_ET;
-    private int count = 0;
+   /* private int count = 0;
     private Button add_employee_btn;
     private Button add_receptionist_btn;
-    private LinearLayout add_emplo_layout, all_search_layout;
+    private LinearLayout add_emplo_layout, all_search_layout;*/
     private TextView no_employee_found_TV_ID;
 
 
     private ArrayList<AddEmployee_PojoClass> addEmployee_pojoClasses;
 
     private All_Employee_List_Adapter all_employee_list_adapter;
+
+    private BoomMenuButton bmb;
 
 
     // Write a message to the database
@@ -82,94 +93,59 @@ public class Employee_list_F extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initialization(view);
         //set focus able
-        plus_icon_button.requestFocus();
         search_ET.requestFocus();
-//        spinner_ET.requestFocus();
-
-        plus_icon_button.setOnClickListener(v -> {
-            count++;
-            if (count % 2 != 0) {
-
-                YoYo.with(Techniques.SlideInDown)
-                        .duration(700)
-                        .repeat(0)
-                        .playOn(add_emplo_layout);
-
-                add_emplo_layout.setVisibility(View.VISIBLE);
-                all_search_layout.setBackgroundColor(Color.parseColor("#000000"));
-//                spinner_ET.setVisibility(View.INVISIBLE);
-//                spinner_ET.setBackgroundColor(Color.parseColor("#010D1B"));
-                search_ET.setVisibility(View.INVISIBLE);
-                search_ET.setBackgroundColor(Color.parseColor("#010D1B"));
-//                plus_icon_button.setImageResource(R.drawable.cross_icon);
-                plus_icon_button.setText("x");
 
 
-//hide key pad start
-                View view1 = getActivity().getCurrentFocus();
-                if (view1 != null) {
-                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(view1.getWindowToken(), 0);
+        assert bmb != null;
+
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            HamButton.Builder builder = new HamButton.Builder();
+            builder.normalImageRes(R.drawable.team_member);
+            builder.buttonHeight(150);
+//            builder.normalColor(Color.parseColor("#000000"));
+            builder.imageRect(new Rect(0, 0, Util.dp2px(60), Util.dp2px(60)));
+            builder.textSize(21);
+            builder.subHighlightedTextColor(Color.parseColor("#FFFFFF"));
+            builder.subNormalTextColor(Color.parseColor("#FFFFFF"));
+            builder.imagePadding(new Rect(10, 10, 10, 10));
+
+            if (i==0){
+                builder.normalText("Add Employee")
+                        .subNormalText("Add employee to your company");
+
+            }
+            if (i==1){
+                builder.normalText("Add Receptionist");
+                builder.subNormalText("Add Receptionist to your company");
+
+            }
+            builder.listener(index -> {
+
+                if (index==0){
+
+                    if (!CheckInternet.isInternet(getContext())) {
+                        Toasty.info(getContext(), "Check internet Connection").show();
+                        return;
+                    }
+                    startActivity(new Intent(getContext(), Add_Employee_Activity_by_Admin.class));
+
+
                 }
-//Hide key pad End
+                if (index==1){
 
-            } else {
+                    if (!CheckInternet.isInternet(getContext())) {
+                        Toasty.info(getContext(), "Check internet Connection").show();
+                        return;
+                    }
+                    startActivity(new Intent(getContext(), Add_Receptionist_Activity_by_Admin.class));
 
-                YoYo.with(Techniques.SlideInUp)
-                        .duration(1000)
-                        .repeat(0)
-                        .playOn(add_emplo_layout);
+                }
+            });
+            bmb.addBuilder(builder);
+        }
+        //end boom menu
 
-                add_emplo_layout.setVisibility(View.GONE);
-                all_search_layout.setBackgroundColor(Color.parseColor("#F5F5F5"));
-//                spinner_ET.setVisibility(View.VISIBLE);
-                search_ET.setVisibility(View.VISIBLE);
 
-                plus_icon_button.requestFocus();
-                search_ET.requestFocus();
-//                spinner_ET.requestFocus();
-
-//                spinner_ET.setBackgroundColor(Color.parseColor("#EEEDEF"));
-                search_ET.setBackgroundColor(Color.parseColor("#EEEDEF"));
-
-//                plus_icon_button.setImageResource(R.drawable.plus);
-                plus_icon_button.setText("+");
-            }
-        });
-        //add employee btn
-        add_employee_btn.setOnClickListener(v -> {
-
-            if (!CheckInternet.isInternet(getContext())) {
-                Toasty.info(getContext(), "Check internet Connection").show();
-                return;
-            }
-            startActivity(new Intent(getContext(), Add_Employee_Activity_by_Admin.class));
-
-         /*   Fragment fragment = new Add_Employee();
-            if (fragment != null) {
-                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.company_main_screen, fragment);
-                fragmentTransaction.addToBackStack("");
-                fragmentTransaction.commit();
-            }*/
-        });
-        //add employee btn END
-        add_receptionist_btn.setOnClickListener(v -> {
-
-            if (!CheckInternet.isInternet(getContext())) {
-                Toasty.info(getContext(), "Check internet Connection").show();
-                return;
-            }
-
-            startActivity(new Intent(getContext(), Add_Receptionist_Activity_by_Admin.class));
-            /*Fragment fragment = new Add_Receptionist();
-            if (fragment != null) {
-                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.company_main_screen, fragment);
-                fragmentTransaction.addToBackStack("");
-                fragmentTransaction.commit();
-            }*/
-        });
 
         Check_User_information check_user_information = new Check_User_information();
         String userID_company = check_user_information.getUserID();
@@ -249,13 +225,13 @@ public class Employee_list_F extends Fragment {
 
     private void initialization(View view) {
         no_employee_found_TV_ID = view.findViewById(R.id.no_employee_found_TV_ID);
-        plus_icon_button = view.findViewById(R.id.plus_icon_button);
+//        plus_icon_button = view.findViewById(R.id.plus_icon_button);
         search_ET = view.findViewById(R.id.search_ET_ID);
-//        spinner_ET = view.findViewById(R.id.spinner_ID);
-        add_employee_btn = view.findViewById(R.id.add_employee_btn);
-        add_receptionist_btn = view.findViewById(R.id.add_receptionist_btn);
-        add_emplo_layout = view.findViewById(R.id.add_emplo_layout);
-        all_search_layout = view.findViewById(R.id.all_search_plus_layout);
+        bmb = view.findViewById(R.id.bmb);
+//        add_employee_btn = view.findViewById(R.id.add_employee_btn);
+//        add_receptionist_btn = view.findViewById(R.id.add_receptionist_btn);
+//        add_emplo_layout = view.findViewById(R.id.add_emplo_layout);
+//        all_search_layout = view.findViewById(R.id.all_search_plus_layout);
 
         // Write a message to the database
         database = FirebaseDatabase.getInstance();
